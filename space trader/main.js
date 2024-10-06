5577a1a652b69a6949ac0d80a6f1032b584441b9
Essem <149950536+Essem8006@users.noreply.github.com>
2024-10-06 13:45:12 +0100

    Update main.js

--- space trader/main.js
+++ space trader/main.js
@@ -1,1 +1,9 @@
-
+if (localStorage.name) {
+  document.getElementById('name').innerHTML = localStorage.name;
+} else {
+  this_good = false;
+  while (!this_good) {
+    localStorage.name = prompt('Name your trading company to start.');
+    this_good = confirm('Are you sure?');
+  }
+}
